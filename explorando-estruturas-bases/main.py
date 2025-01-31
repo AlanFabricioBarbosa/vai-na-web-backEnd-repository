1cee986228ed28cb8a014d855126d8e3614a8db0
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-01-31 10:45:27 -0300

    :sparkles: Aula 31/01/2025 - Entendo como o py é estruturado

---
+++ explorando-estruturas-bases/main.py
@@ -0,0 +1,28 @@
+idade = 26 # Number
+nome = "Alan" # String
+nome_sobrenome = "Fabrício" #Demostrando o uso do snake case
+altura = 1.85 # Float
+estudante = True # Boolean
+graus = -10 # Mesmo com valornegativo é entendido como numero inteiro
+preco = 3.15
+dia = None # Tipo nulo
+
+primeiro_valor = 2.2
+segundo_valor = 2.1
+#print(primeiro_valor + segundo_valor)
+frase = "Lindo dia!!"
+#print(type(idade), type(nome), type(dia))
+
+#nome = input("Qual o seu nome?")
+#print("Olá " + nome)
+
+#email = input("Digite seu email: ")
+#senha = input("Digite sua senha: ")
+#print(f"\nSeu email é: {email}\nSua senha é: {senha}")
+
+primeiro_valor = input("Digite um numero: ")
+segundo_valor = input("Digite um numero: ")
+
+soma = int(primeiro_valor) + int(segundo_valor)
+
+print(f'A soma dos valores é: {soma}')
